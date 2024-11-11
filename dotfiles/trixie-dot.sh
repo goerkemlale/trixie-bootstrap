e1825be4c88a078e992eeb97c1cf8b5fb45683e0
goerkemlale <goerkemlale@gmail.com>
2024-11-11 16:13:15 +0300

    Add dotfiles section

---
+++ dotfiles/trixie-dot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# trixie-dot.sh
+
+# Neovim Configuration
---
+++ sys/coding/install_neovim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Change Working Directory for external sh call
+cd "$(dirname "$0")"
+
+# Install NeoVim from Git
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+export PATH="$PATH:/opt/nvim-linux64/bin"
